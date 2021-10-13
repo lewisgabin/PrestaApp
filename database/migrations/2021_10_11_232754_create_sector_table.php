f3478fce909415b37aeef93651b38c5722c9b4a0
Estiven Padilla <estivenps26@gmail.com>
2021-10-12 23:06:11 -0400

    Migracion sector

---
+++ database/migrations/2021_10_11_232754_create_sector_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSectorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sector', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->unsignedBigInteger('id_municipio');
+            $table->foreign("id_municipio")
+            ->references("id")
+            ->on("municipio")
+            ->onDelete("cascade")
+            ->onUpdate("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sector');
+    }
+}
